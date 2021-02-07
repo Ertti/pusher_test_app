f5a7f42b02b0be3b8bdf8f18eedfba7d11efddcb
DESKTOP-1MSBE7D\maaax <maaax1333@gmail.com>
2021-02-07 18:40:33 +0300

    no message

--- src/Application.kt
+++ src/Application.kt
@@ -2,15 +2,16 @@ package com.herokuapp.pushertestapp
 
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.*
+import io.ktor.features.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-
+    install(DefaultHeaders)
+    install(ConditionalHeaders)
 
     routing {
         get("/") {
